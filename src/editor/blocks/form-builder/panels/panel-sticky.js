dd2a69754dfaf3554bad127db11c5df685cce0ba
Arya1Putra <putra1arya@gmail.com>
2024-01-05 17:35:55 +0800

    fix sticky form builder

--- src/editor/blocks/form-builder/panels/panel-sticky.js
+++ src/editor/blocks/form-builder/panels/panel-sticky.js
@@ -1,13 +1,11 @@
 import { __ } from '@wordpress/i18n';
-import { LockedSwitchControl } from 'gutenverse-core/controls';
+import { LockedProStickyControl, LockedSwitchControl } from 'gutenverse-core/controls';
 import { applyFilters } from '@wordpress/hooks';
 
 export const stickyPanel = (props) => {
     const stickyOption = [
         {
-            id: 'sticky',
-            label: __('Enable Sticky', 'gutenverse-form'),
-            component: LockedSwitchControl,
+            component: LockedProStickyControl,
         },
     ];
 
