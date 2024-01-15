0a934dbef5662b3a40581726f167ca1a8110a0ca
Gowida <gowinda@jegstudio.com>
2024-01-15 13:49:12 +0800

    update card banner in form

--- src/backend/form/src/form-content.js
+++ src/backend/form/src/form-content.js
@@ -216,7 +216,7 @@ export const FormContent = (props) => {
             </AlertControl>
         </div>}
         <div className="form-notice-wrapper">
-            <CardBannerPro title={__('Upgrade to Gutenverse Pro', 'gutenverse-form')} description={__('Explore the full potential of Gutenverse Form', 'gutenverse-form')} />
+            <CardBannerPro title={__('Upgrade to Gutenverse Pro', 'gutenverse-form')} description={__('Explore the full potential of Gutenverse Form', 'gutenverse-form')} backgroundImg = "bg-form.png"/>
         </div>
         <div className="form-tab-header">
             {Object.keys(tabs).map(key => {
