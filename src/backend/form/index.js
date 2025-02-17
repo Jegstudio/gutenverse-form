813ccd7515eff00f686a62893e22360260c38858
Agung Bayu <agungbayuiswara@gmail.com>
2025-02-17 11:29:15 +0800

    [main] fix deprecated render on form editor

--- src/backend/form/index.js
+++ src/backend/form/index.js
@@ -1,19 +1,21 @@
 
-import { render } from '@wordpress/element';
+import { createRoot } from '@wordpress/element';
 import CreateFormAction from './src/create-form-action';
 import EditFormAction from './src/edit-form-action';
 import { doAction } from '@wordpress/hooks';
+import domReady from '@wordpress/dom-ready';
 
 // Render Form
-if (document.getElementById('gutenverse-form-action')) {
-    document.addEventListener('DOMContentLoaded', () => {
+domReady(() => {
+    const formElement = document.getElementById('gutenverse-form-action');
+    if (formElement) {
         doAction('gutenverse.before.register.form');
-        render(
+        const formRoot = createRoot(formElement);
+        formRoot.render(
             <>
                 <CreateFormAction />
                 <EditFormAction />
-            </>,
-            document.getElementById('gutenverse-form-action')
+            </>
         );
-    });
-}
+    }
+});
