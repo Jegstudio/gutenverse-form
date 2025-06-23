40f1b0fb9247b88c4c66cdc58d62f4a90e245c1f
Arya1Putra <putra1arya@gmail.com>
2025-06-23 13:16:30 +0800

    [fix/fix-bug] fix some variable that used but undefined

--- src/editor/blocks/form-input-email/save.js
+++ src/editor/blocks/form-input-email/save.js
@@ -3,6 +3,7 @@ import SaveInputWrapper from '../form-input/general/save-input-wrapper';
 import isEmpty from 'lodash/isEmpty';
 import { withMouseMoveEffectScript } from 'gutenverse-core/hoc';
 import { compose } from '@wordpress/compose';
+import { getImageSrc } from 'gutenverse-core/editor-helper';
 
 const WrapAHref = ({ attributes, children }) => {
     const {
@@ -41,7 +42,8 @@ const save = compose(
         image,
         iconStyleMode,
         iconType,
-        icon
+        icon,
+        lazyLoad
     } = attributes;
 
     const validation = {
