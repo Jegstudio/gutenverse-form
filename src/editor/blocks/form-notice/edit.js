5516148a1cc20f02191d532b0a03b37f6f70114a
Agung Bayu <agungbayuiswara@gmail.com>
2026-05-15 23:00:18 +0800

    [dev/improve-performance] for notice svg fallback

--- src/editor/blocks/form-notice/edit.js
+++ src/editor/blocks/form-notice/edit.js
@@ -1,182 +1,191 @@
-import { compose } from "@wordpress/compose";
+import { compose } from '@wordpress/compose';
 import {
-	withMouseMoveEffect,
-	withPartialRender,
-	withPassRef,
-} from "gutenverse-core/hoc";
-import { panelList } from "./panels/panel-list";
-import { useRef, useMemo } from "@wordpress/element";
+    withMouseMoveEffect,
+    withPartialRender,
+    withPassRef,
+} from 'gutenverse-core/hoc';
+import { panelList } from './panels/panel-list';
+import { useRef, useMemo } from '@wordpress/element';
 
-import { useAnimationEditor, useDisplayEditor } from "gutenverse-core/hooks";
-import classnames from "classnames";
-import { renderIcon } from "gutenverse-core/helper";
-import { useDynamicStyle, useGenerateElementId } from "gutenverse-core/styling";
-import getBlockStyle from "./styles/block-style";
-import { BlockPanelController, PanelTutorial } from "gutenverse-core/controls";
-import { CopyElementToolbar } from "gutenverse-core/components";
+import { useAnimationEditor, useDisplayEditor, useInitializeIconToSvg } from 'gutenverse-core/hooks';
+import classnames from 'classnames';
+import { renderIcon } from 'gutenverse-core/helper';
+import { useDynamicStyle, useGenerateElementId } from 'gutenverse-core/styling';
+import getBlockStyle from './styles/block-style';
+import { BlockPanelController, PanelTutorial } from 'gutenverse-core/controls';
+import { CopyElementToolbar } from 'gutenverse-core/components';
 
-import { useBlockProps, RichText, InspectorControls } from "@wordpress/block-editor";
-import { __ } from "@wordpress/i18n";
-import { useSelect } from "@wordpress/data";
+import { useBlockProps, RichText, InspectorControls } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+import { useSelect } from '@wordpress/data';
 import FormNavigation from '../form-input/general/form-navigation';
 
 const FormNoticeBlock = compose(
-	withMouseMoveEffect,
-	withPartialRender,
-	withPassRef,
+    withMouseMoveEffect,
+    withPartialRender,
+    withPassRef,
 )((props) => {
-	const { attributes, setAttributes, clientId } = props;
+    const { attributes, setAttributes, clientId } = props;
 
-	const {
-		elementId,
-		noticeStatus = 'success',
-		messageSource,
-		successMessage,
-		errorMessage,
-		iconSuccess,
-		iconError,
-		iconSuccessType,
-		iconErrorType,
-		iconSuccessSVG,
-		iconErrorSVG,
-		iconLayoutSuccess,
-		iconLayoutError,
-		hideNoticeEditor,
-	} = attributes;
+    const {
+        elementId,
+        noticeStatus = 'success',
+        messageSource,
+        successMessage,
+        errorMessage,
+        iconSuccess,
+        iconError,
+        iconSuccessType,
+        iconErrorType,
+        iconSuccessSVG,
+        iconErrorSVG,
+        iconLayoutSuccess,
+        iconLayoutError,
+        hideNoticeEditor,
+    } = attributes;
 
-	const currentIcon = noticeStatus === "success" ? iconSuccess : iconError;
-	const currentIconType =
-		noticeStatus === "success" ? iconSuccessType : iconErrorType;
-	const currentIconSVG =
-		noticeStatus === "success" ? iconSuccessSVG : iconErrorSVG;
-	const currentIconLayout =
-		noticeStatus === "success" ? iconLayoutSuccess : iconLayoutError;
+    const currentIcon = noticeStatus === 'success' ? iconSuccess : iconError;
+    const currentIconType =
+        noticeStatus === 'success' ? iconSuccessType : iconErrorType;
+    const currentIconSVG =
+        noticeStatus === 'success' ? iconSuccessSVG : iconErrorSVG;
+    const currentIconLayout =
+        noticeStatus === 'success' ? iconLayoutSuccess : iconLayoutError;
 
-	const elementRef = useRef();
-	const animationClass = useAnimationEditor(attributes);
-	const displayClass = useDisplayEditor(attributes);
+    const elementRef = useRef();
+    const animationClass = useAnimationEditor(attributes);
+    const displayClass = useDisplayEditor(attributes);
 
-	useGenerateElementId(clientId, elementId, elementRef);
-	useDynamicStyle(elementId, attributes, getBlockStyle, elementRef);
+    useGenerateElementId(clientId, elementId, elementRef);
+    useDynamicStyle(elementId, attributes, getBlockStyle, elementRef);
+    useInitializeIconToSvg({
+        elementId,
+        attributes,
+        setAttributes,
+        icons: [
+            { type: 'iconSuccessType', svg: 'iconSuccessSVG' },
+            { type: 'iconErrorType', svg: 'iconErrorSVG' },
+        ],
+    });
 
-	const { formData } = useSelect(
-		(select) => {
-			const { getBlockRootClientId, getBlockAttributes } =
-				select("core/block-editor");
-			const rootClientId = getBlockRootClientId(clientId);
-			let data = null;
-			if (rootClientId) {
-				data = getBlockAttributes(rootClientId);
-			}
-			return {
-				formData: data,
-			};
-		},
-		[clientId],
-	);
+    const { formData } = useSelect(
+        (select) => {
+            const { getBlockRootClientId, getBlockAttributes } =
+                select('core/block-editor');
+            const rootClientId = getBlockRootClientId(clientId);
+            let data = null;
+            if (rootClientId) {
+                data = getBlockAttributes(rootClientId);
+            }
+            return {
+                formData: data,
+            };
+        },
+        [clientId],
+    );
 
-	const displayMessage = useMemo(() => {
-		if (messageSource === "custom") {
-			return noticeStatus === "success" ? successMessage : errorMessage;
-		}
+    const displayMessage = useMemo(() => {
+        if (messageSource === 'custom') {
+            return noticeStatus === 'success' ? successMessage : errorMessage;
+        }
 
-		if (formData) {
-			return noticeStatus === "success"
-				? formData.form_success_notice ||
-						__(
-							"Success message defined in Form Builder",
-							"gutenverse-form",
-						)
-				: formData.form_error_notice ||
-						__(
-							"Error message defined in Form Builder",
-							"gutenverse-form",
-						);
-		}
+        if (formData) {
+            return noticeStatus === 'success'
+                ? formData.form_success_notice ||
+                __(
+                    'Success message defined in Form Builder',
+                    'gutenverse-form',
+                )
+                : formData.form_error_notice ||
+                __(
+                    'Error message defined in Form Builder',
+                    'gutenverse-form',
+                );
+        }
 
-		return noticeStatus === "success" ? successMessage : errorMessage;
-	}, [messageSource, noticeStatus, successMessage, errorMessage, formData]);
+        return noticeStatus === 'success' ? successMessage : errorMessage;
+    }, [messageSource, noticeStatus, successMessage, errorMessage, formData]);
 
-	const blockProps = useBlockProps({
-		ref: elementRef,
-		className: classnames(
-			"guten-element",
-			"guten-form-notice",
-			hideNoticeEditor ? "" : "show-notice",
-			`status-${noticeStatus}`,
-			`notice-${noticeStatus}`,
-			elementId,
-			animationClass,
-			displayClass,
-		),
-	});
+    const blockProps = useBlockProps({
+        ref: elementRef,
+        className: classnames(
+            'guten-element',
+            'guten-form-notice',
+            hideNoticeEditor ? '' : 'show-notice',
+            `status-${noticeStatus}`,
+            `notice-${noticeStatus}`,
+            elementId,
+            animationClass,
+            displayClass,
+        ),
+    });
 
-	return (
-		<>
-			<CopyElementToolbar {...props} />
-			<FormNavigation clientId={clientId} />
-			<InspectorControls>
-				<PanelTutorial
-					title={__('Remove notice?', 'gutenverse-form')}
-					list={[
-						{
-							title: __('Notice still appears after removing the "Form Notice"?', 'gutenverse-form'),
-							description: __('Your form action may have a default notice message. Go to Admin Dashboard > Form > Form Action and delete the notice text.', 'gutenverse-form')
-						},
-					]}
-					openDefault={false}
-				/>
-			</InspectorControls>
-			<BlockPanelController
-				panelList={panelList}
-				props={props}
-				elementRef={elementRef}
-			/>
-			<div {...blockProps}>
-				<FormNavigation clientId={clientId} helperPlacement="inside" />
-				<div
-					className={classnames(
-						"guten-form-notice-wrapper",
-						`layout-${currentIconLayout}`,
-					)}
-				>
-					{currentIcon && (
-						<div className="guten-form-notice-icon">
-							{renderIcon(
-								currentIcon,
-								currentIconType,
-								currentIconSVG,
-							)}
-						</div>
-					)}
+    return (
+        <>
+            <CopyElementToolbar {...props} />
+            <FormNavigation clientId={clientId} />
+            <InspectorControls>
+                <PanelTutorial
+                    title={__('Remove notice?', 'gutenverse-form')}
+                    list={[
+                        {
+                            title: __('Notice still appears after removing the "Form Notice"?', 'gutenverse-form'),
+                            description: __('Your form action may have a default notice message. Go to Admin Dashboard > Form > Form Action and delete the notice text.', 'gutenverse-form')
+                        },
+                    ]}
+                    openDefault={false}
+                />
+            </InspectorControls>
+            <BlockPanelController
+                panelList={panelList}
+                props={props}
+                elementRef={elementRef}
+            />
+            <div {...blockProps}>
+                <FormNavigation clientId={clientId} helperPlacement="inside" />
+                <div
+                    className={classnames(
+                        'guten-form-notice-wrapper',
+                        `layout-${currentIconLayout}`,
+                    )}
+                >
+                    {currentIcon && (
+                        <div className="guten-form-notice-icon">
+                            {renderIcon(
+                                currentIcon,
+                                currentIconType,
+                                currentIconSVG,
+                            )}
+                        </div>
+                    )}
 
-					<div className="guten-form-notice-content">
-						{messageSource === "custom" ? (
-							<RichText
-								tagName="div"
-								value={displayMessage}
-								onChange={(value) => {
-									if (noticeStatus === "success") {
-										setAttributes({
-											successMessage: value,
-										});
-									} else {
-										setAttributes({ errorMessage: value });
-									}
-								}}
-							/>
-						) : (
-							<div
-								dangerouslySetInnerHTML={{
-									__html: displayMessage,
-								}}
-							/>
-						)}
-					</div>
-				</div>
-			</div>
-		</>
-	);
+                    <div className="guten-form-notice-content">
+                        {messageSource === 'custom' ? (
+                            <RichText
+                                tagName="div"
+                                value={displayMessage}
+                                onChange={(value) => {
+                                    if (noticeStatus === 'success') {
+                                        setAttributes({
+                                            successMessage: value,
+                                        });
+                                    } else {
+                                        setAttributes({ errorMessage: value });
+                                    }
+                                }}
+                            />
+                        ) : (
+                            <div
+                                dangerouslySetInnerHTML={{
+                                    __html: displayMessage,
+                                }}
+                            />
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 });
 
 export default FormNoticeBlock;
