2f4c1c3b2359abbd351bfa29e6cd2195a1db2b9b
Yoga <yoga@jegstudio.com>
2026-08-13 13:10:54 +0800

    [fix/form-v3-issues] fix form data invalid

--- gutenverse-form/includes/class-form-validation.php
+++ gutenverse-form/includes/class-form-validation.php
@@ -51,10 +51,11 @@ class Form_Validation extends Style_Generator {
 	 * Constructor
 	 */
 	public function __construct() {
-		add_action( 'wp_enqueue_scripts', array( $this, 'form_validation_scripts' ), 99999 );
+		add_action( 'wp_footer', array( $this, 'form_validation_scripts' ), 19 );
 		add_filter( 'gutenverse_bypass_generate_style', array( $this, 'bypass_generate_css' ), 20, 2 );
 		add_action( 'gutenverse_loop_blocks', array( $this, 'loop_blocks' ), null, 2 );
 		add_action( 'gutenverse_after_style_loop_blocks', array( $this, 'get_blocks' ), null );
+		add_filter( 'render_block', array( $this, 'collect_rendered_form_builder' ), 9, 2 );
 	}
 
 	/**
@@ -67,9 +68,8 @@ class Form_Validation extends Style_Generator {
 			if ( $this->form_validation_data ) {
 				$cache->create_cache_file( $this->file_name, wp_json_encode( $validation_data, true ) );
 			}
-			$this->form_file[]          = $this->file_name;
-			$this->form_validation_data = array();
-			$this->is_bypass            = false;
+			$this->form_file[] = $this->file_name;
+			$this->is_bypass   = false;
 		}
 	}
 
@@ -144,9 +144,13 @@ class Form_Validation extends Style_Generator {
 	 * @param array $form_data Form Data.
 	 */
 	public function localize_validation_data( $form_data ) {
-		$form_result = array();
-		$include_user_data = apply_filters( 'gutenverse_form_localize_frontend_user_data', false );
-		$user_data         = null;
+		$form_data            = is_array( $form_data ) ? $form_data : array();
+		$form_validation_data = is_array( $this->form_validation_data ) ? $this->form_validation_data : array();
+		$form_data            = array_merge( $form_data, $form_validation_data );
+		$form_data            = array_values( array_unique( array_filter( array_map( 'absint', $form_data ) ) ) );
+		$form_result          = array();
+		$include_user_data    = apply_filters( 'gutenverse_form_localize_frontend_user_data', false );
+		$user_data            = null;
 
 		if ( $include_user_data && is_user_logged_in() ) {
 			$user_data = array(
@@ -161,9 +165,8 @@ class Form_Validation extends Style_Generator {
 		}
 
 		if ( ! empty( $form_data ) ) {
-
 			foreach ( $form_data as $form_id ) {
-				$post_type = get_post_type( (int) $form_id );
+				$post_type = get_post_type( $form_id );
 
 				if ( 'gutenverse-form' === $post_type ) {
 					$result = array(
@@ -172,7 +175,8 @@ class Form_Validation extends Style_Generator {
 						'logged_in'     => is_user_logged_in(),
 					);
 
-					$data                          = get_post_meta( (int) $form_id, 'form-data', true );
+					$data                          = get_post_meta( $form_id, 'form-data', true );
+					$data                          = is_array( $data ) ? $data : array();
 					$result['require_login']       = isset( $data['require_login'] ) ? $data['require_login'] : false;
 					$result['form_success_notice'] = isset( $data['form_success_notice'] ) ? $data['form_success_notice'] : false;
 					$result['form_error_notice']   = isset( $data['form_error_notice'] ) ? $data['form_error_notice'] : false;
@@ -192,6 +196,7 @@ class Form_Validation extends Style_Generator {
 				'missingLabel' => esc_html__( 'Form action is missing, please assign form action into this form.', 'gutenverse-form' ),
 				'isAdmin'      => $include_user_data && current_user_can( 'manage_options' ),
 				'userData'     => $user_data,
+				'additional_check'  => $include_user_data,
 			)
 		);
 	}
@@ -204,24 +209,86 @@ class Form_Validation extends Style_Generator {
 	 */
 	public function get_form_data( $block ) {
 		if ( 'gutenverse/form-builder' === $block['blockName'] ) {
+			$this->add_form_id( $this->get_form_id_from_block( $block ) );
+		}
+	}
+
+	/**
+	 * Collect rendered form builder data for frontend validation.
+	 *
+	 * @param string $block_content Block content.
+	 * @param array  $block         Block data.
+	 *
+	 * @return string
+	 */
+	public function collect_rendered_form_builder( $block_content, $block ) {
+		if ( isset( $block['blockName'] ) && 'gutenverse/form-builder' === $block['blockName'] ) {
 			if ( isset( $block['attrs']['formId'] ) ) {
-				$form_attr = $block['attrs']['formId'];
-				$form_id   = null;
-
-				if ( is_array( $form_attr ) && isset( $form_attr['value'] ) ) {
-					$form_id = $form_attr['value'];
-				} elseif ( is_scalar( $form_attr ) ) {
-					$form_id = $form_attr;
-				}
-
-				if ( empty( $form_id ) ) {
-					return;
-				}
-
-				if ( ! in_array( $form_id, $this->form_validation_data, true ) ) {
-					$this->form_validation_data[] = $form_id;
-				}
+				$this->add_form_id( $block['attrs']['formId'] );
+			} else {
+				$this->add_form_id( $this->get_form_id_from_content( $block_content ) );
 			}
 		}
+
+		return $block_content;
+	}
+
+	/**
+	 * Add form ID to validation data.
+	 *
+	 * @param array|int|string $form_id Form ID.
+	 */
+	public function add_form_id( $form_id ) {
+		if ( is_array( $form_id ) && isset( $form_id['value'] ) ) {
+			$form_id = $form_id['value'];
+		}
+
+		$form_id = absint( $form_id );
+
+		if ( $form_id && ! in_array( $form_id, $this->form_validation_data, true ) ) {
+			$this->form_validation_data[] = $form_id;
+		}
+	}
+
+	/**
+	 * Get form ID from rendered form markup.
+	 *
+	 * @param string $block_content Block content.
+	 *
+	 * @return int
+	 */
+	private function get_form_id_from_content( $block_content ) {
+		if ( class_exists( 'WP_HTML_Tag_Processor' ) ) {
+			$processor = new \WP_HTML_Tag_Processor( $block_content );
+
+			if ( $processor->next_tag( 'form' ) ) {
+				return absint( $processor->get_attribute( 'data-form-id' ) );
+			}
+		} elseif ( preg_match( '/<form\b[^>]*\sdata-form-id=["\']?([0-9]+)/i', $block_content, $matches ) ) {
+			return absint( $matches[1] );
+		}
+
+		return 0;
+	}
+
+	/**
+	 * Get form ID from parsed form builder block.
+	 *
+	 * @param array $block Parsed block.
+	 *
+	 * @return int
+	 */
+	private function get_form_id_from_block( $block ) {
+		if ( ! isset( $block['attrs']['formId'] ) ) {
+			return 0;
+		}
+
+		$form_attr = $block['attrs']['formId'];
+
+		if ( is_array( $form_attr ) && isset( $form_attr['value'] ) ) {
+			return absint( $form_attr['value'] );
+		}
+
+		return is_scalar( $form_attr ) ? absint( $form_attr ) : 0;
 	}
 }
